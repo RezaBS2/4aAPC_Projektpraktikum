0038f15f22c0ffbded55b95fedd35b692343d3a6
Vakilzadeh Reza <Reza.Vakilzadeh@BSLINZ2-IT.LOCAL>
2023-05-11 09:08:25 +0200

    Added login files

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+$str = "Mary Had 5 Little Lambs and She LOVED HTEMöüäöÖÖÄÜÚ So";
+//$str_converted = mb_convert_case($str, MB_CASE_LOWER, "UTF-8");
+$str_converted = strtolower($str);
+
+echo $str_converted;
+
+
+
+$email = "";
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+
+echo "test";
+?>
---
+++ mainlogin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login - Preisfinder</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <div class="login">
+        <h1>Preisfinder</h1>
+        <h2>Login</h2>
+        <form action="authenticate.php" method="post">
+            <br>
+            <label for="username">
+                <i class="fas fa-user"></i>
+            </label>
+            <br>
+            <input type="text" name="username" placeholder="Username" id="username" required>
+            <br>
+            <label for="password">
+                <i class="fas fa-lock"></i>
+            </label>
+            <br>
+            <input type="password" name="password" placeholder="Password" id="password" required>
+            <br>
+            <input type="submit" value="Login">
+            <br><br>
+        </form>
+    </div>
+    <div>
+        <button onclick="window.location.href='https://w3docs.com';">Ohne Anmeldung fortfahren</button>
+    </div>
+
+    <?php
+        //include 'config.php';
+        include 'login.php';
+    ?>
+
+</body>
+
+</html>
