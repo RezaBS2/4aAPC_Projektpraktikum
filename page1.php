4838d179396cdf3ecd1eea6a77a1e67cabc3f9dd
Reza.Vakilzadeh <Reza.Vakilzadeh@students.bs-linz2.ac.at>
2023-06-27 14:57:35 +0200

    Diagramm button so verändert dass es nur bei Ergebnissen angezeigt wird

--- page1.php
+++ page1.php
@@ -459,8 +459,6 @@ if(session_status() === PHP_SESSION_NONE)
                                                 </div>';
 
                                             }
-
-
                                             ?>
 
 
