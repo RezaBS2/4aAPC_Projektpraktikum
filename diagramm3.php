add3cd81244ffcf99f527a7fbde322dc2fa87077
Reza.Vakilzadeh <Reza.Vakilzadeh@students.bs-linz2.ac.at>
2023-06-28 16:00:34 +0200

    diagramm3.php verbessert

--- diagramm3.php
+++ diagramm3.php
@@ -9,11 +9,13 @@ if(session_status() === PHP_SESSION_NONE)
 }
 
 /**/
-$cn = $_SESSION['currentcompany'];
+//$cn = $_SESSION['currentcompany'];
+$cn = $_SESSION['SearchResults'][$_GET['producktArr_id']][1];
 //$cn = "Mediamarkt Pluscity";
 $comp_id = return_comp_id($cn); //comp_id
 
-$pn = $_SESSION['currentproduct'];
+//$pn = $_SESSION['currentproduct'];
+$pn = $_SESSION['SearchResults'][$_GET['producktArr_id']][0];
 //$pn = "iPhone 13 Pro Max";
 $prod_id = return_prod_id($pn);
 
@@ -30,23 +32,24 @@ $prod_comp_id = return_prod_comp_id($prod_id, $comp_id);
     array("y" => 20, "label" => "26.06.2023"),
     array("y" => 5, "label" => "27.06.2023")
 );*/
-//$dataPoints = returnValuesDiagrammInArray($prod_comp_id, date("Y-m-d"));
-$dataPoints = returnValuesDiagrammInArray(4517, "2026-06-06");
+$dataPoints = returnValuesDiagrammInArray($prod_comp_id, date("Y-m-d"));
+//$dataPoints = returnValuesDiagrammInArray(4517, "2026-06-06");
 
 if ($dataPoints == -1)
 {
     $dataPoints = 0;
 }
 
-$max = returnMaxPriceForprod_comp_id(4517);
+//$max = returnMaxPriceForprod_comp_id(4517);
 $max = returnMaxPriceForprod_comp_id($prod_comp_id);
 
 ?>
     <!DOCTYPE HTML>
     <html>
-
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
     <script>
+
         var maxPHP = "<?php echo $max; ?>";
         var maxPHPplus = "<?php echo $max + $max/10; ?>";
         window.onload = function () {
@@ -102,6 +105,7 @@ $max = returnMaxPriceForprod_comp_id($prod_comp_id);
 
 
     <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+    <br>
     <br>
     <div>
         <a class="nav-link " href="page1.php">
